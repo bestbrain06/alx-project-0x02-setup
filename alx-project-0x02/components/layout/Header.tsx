4bca468a8e943bc8c451d9c82cef333581c3f196
chidera ikechukwu <bestbrain407@gmail.com>
2025-10-20 19:59:16 +0000

    added navigation for home

--- alx-project-0x02/components/layout/Header.tsx
+++ alx-project-0x02/components/layout/Header.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 const Header = () => {
   return (
     <div className="container-screen flex justify-between">
-      <Link href="/">Home</Link>
+      <Link href="/home">Home</Link>
       <Link href="/about">About</Link>
     </div>
   );
