7e5c7d137a200f2be71a9cfb21c2ffedd9ecdd61
chidera ikechukwu <bestbrain407@gmail.com>
2025-10-21 06:35:12 +0000

    added header on posts

--- alx-project-0x02/pages/posts.tsx
+++ alx-project-0x02/pages/posts.tsx
@@ -1,5 +1,13 @@
-const Posts = () => {
-  return <div>Post</div>;
+import Header from "@/components/layout/Header";
+import React from "react";
+
+const Posts: React.FC = () => {
+  return (
+    <>
+      <Header />
+      <div>Post</div>
+    </>
+  );
 };
 
 export default Posts;
